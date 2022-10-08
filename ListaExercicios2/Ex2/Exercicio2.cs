9167f5f6cb29882fccb2139efd4d4e1a374334f8
riannovais <riannovais3@gmail.com>
2022-10-08 11:57:10 -0300

    insercao das duas primeiras listas de exercicio

---
+++ ListaExercicios2/Ex2/Exercicio2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Ex2;
+
+internal class Exercicio2{
+    /*Escreva um algoritmo que informe todos os múltiplos de 17 ou de 31 compreendidos
+        no intervalo entre 500 e 1000.*/
+
+    static void Main(String[] args){
+
+        int a;
+        int b;
+
+        for (a = 500; a <=1000; a++)
+            if(a % 17 == 0){
+                Console.WriteLine("Multiplo de 17: " + a);
+            }
+        for (b = 500; b <=1000; b++){
+            if(b % 31 == 0){
+                Console.WriteLine("Multiplo de 31: " + b);
+
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+        
+    }
+        
+
+
+       
+
+    
+        
+
+   
+        
+
+    
+
+        
+  
