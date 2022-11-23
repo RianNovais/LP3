82385f00a760f28e859268d2f602f4e5d3916b29
Rian Novais <riannovais3@gmail.com>
2022-11-23 15:35:18 -0300

    inserçao do formulario - PROTOTIPO

---
+++ FormularioTeste/Form1.cs
@@ -0,0 +1,55 @@
+namespace FormularioTeste
+{
+    public partial class Form1 : Form
+    {
+        String login;
+        String senha;
+        String status;
+        String tipo;
+        String data;
+        String perfil;
+        String obs;
+        String modulos;
+
+        List<CheckBox> mod = new List<CheckBox>();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            mod.Add(cbCompras);
+            mod.Add(cbBiblioteca);
+            mod.Add(cbAcademico);
+            mod.Add(cbDocumentacao);
+            mod.Add(cbRH);
+        }
+
+        private void btCadastrar_Click(object sender, EventArgs e)
+        {
+            //COLETANDO DADOS DO FORMULARIOS, INSERIDOS NOS TEXTBOX, RADIOBUTTON, COMBOBOX E CHECKBOX.
+            login = txtLogin.Text;
+            senha = txtSenha.Text;
+            status = cbStatus.Text;
+            tipo = groupBox1.Controls.OfType<RadioButton>().SingleOrDefault(RadioButton => RadioButton.Checked).Text;
+            data = txtData.Text;
+            perfil = groupBox2.Controls.OfType<RadioButton>().SingleOrDefault(RadioButton => RadioButton.Checked).Text;
+            obs = txtObs.Text;
+
+            modulos = "";
+
+           //USANDO O FOREACH PARA COLETAR DADOS DO CHECKBOX UTILIZANDO UMA LISTA.
+
+            foreach(CheckBox t in mod){ 
+                if (t.Checked){
+                    modulos += t.Text + ", ";
+                }
+            
+            }
+
+           
+
+
+            MessageBox.Show(login + " " + senha + " " + status + "" + tipo + "" + data + "" + perfil + " " + modulos);
+            
+        }
+    }
+}
